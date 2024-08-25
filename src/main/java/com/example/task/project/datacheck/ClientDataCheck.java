cb1baa6a48afd7b80c143646090794bd85f8c65d
The-D-007 <ishantbishnoi07@gmail.com>
2024-08-25 00:50:25 -0400

    Client Pojo class created

---
+++ src/main/java/com/example/task/project/datacheck/ClientDataCheck.java
@@ -0,0 +1,35 @@
+package com.example.task.project.datacheck;
+
+import com.example.task.project.customexceptions.InvalidClientDataException;
+import com.example.task.project.entity.Client;
+import com.example.task.project.pojo.ClientPojo;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+public class ClientDataCheck {
+
+    private final Client client = new Client();
+
+    public Client checkClientSignupData(ClientPojo clientPojo) throws InvalidClientDataException {
+        if (!StringUtils.hasText(clientPojo.getUserName()) || !StringUtils.hasText(clientPojo.getPassword()) ){
+            throw new InvalidClientDataException("Username and password must not be null or empty");
+        }
+        if (!clientPojo.getEmail().matches("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")){
+            throw new InvalidClientDataException("Email address must be right");
+        }
+       client.setUserName(clientPojo.getUserName());
+        client.setPassword(clientPojo.getPassword());
+        client.setEmail(clientPojo.getEmail());
+        return client;
+    }
+
+    public Client checkClientLoginData(ClientPojo clientPojo) throws InvalidClientDataException {
+        if (!StringUtils.hasText(clientPojo.getUserName()) || !StringUtils.hasText(clientPojo.getPassword())) {
+            throw new InvalidClientDataException("Username and password must not be null or empty");
+        }
+        client.setUserName(clientPojo.getUserName());
+        client.setPassword(clientPojo.getPassword());
+        return client;
+    }
+}
